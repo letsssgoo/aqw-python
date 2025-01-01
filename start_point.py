45bfb9a58f0298cac02479930d68e21a9dc94a2e
YapZanan <naufalsetiawan64@gmail.com>
2025-01-01 23:27:16 +0700

    add custom print

--- start_point.py
+++ start_point.py
@@ -5,6 +5,19 @@ from core.bot import Bot
 from templates.attack import generalAttack
 import commands as cmd
 import asyncio
+import builtins
+import sys
+
+# Define a custom print function that includes flush=True
+# IDK why but Docker logs doesnt work without this
+def custom_print(*args, **kwargs):
+    kwargs['flush'] = True
+    # Use sys.stdout.write to avoid recursion
+    sys.stdout.write(" ".join(map(str, args)) + "\n")
+    sys.stdout.flush()
+
+# Replace the built-in print function with the custom print
+builtins.print = custom_print
 
 # Load environment variables from .env file
 load_dotenv()
@@ -14,6 +27,7 @@ load_dotenv()
 username = os.getenv("USERNAME_AQW")
 password = os.getenv("PASSWORD_AQW")
 server = os.getenv("SERVER")
+
 
 # directly
 # username = "username"
@@ -43,7 +57,8 @@ b = Bot(
     farmClass="Legion Revenant")
 b.set_login_info(username, password, server)
 
-# this is the path refer to /bot
+
+print(f"Username: {username}, Password: {password}, Server: {server}")
 bot_path = "bot.void_aura"
 try:
     bot_class = importlib.import_module(bot_path)
